19b7a066556857200ca4b74099a55e4a90029760
MayankHenry <mayank.gla_cs.aiml24@gla.ac.in>
2026-02-19 09:29:55 +0530

    update RGB LED control with pin definitions and PWM functionality; add configuration files

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,68 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Mayank
+ * @date 2026-01-22
  *
  * @details
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
-
- // TODO 2:
- // Define GREEN pin (Use 10)
-
- // TODO 3:
- // Define BLUE pin (Use 11)
+// Define RGB pins (PWM-capable pins on typical Arduino Uno)
+const int RED_PIN = 9;
+const int GREEN_PIN = 10;
+const int BLUE_PIN = 11;
 
 void setup() {
 
-    // TODO 4:
     // Initialize Serial communication (9600 baud)
+    Serial.begin(9600); // TODO 4
 
-    // TODO 5:
     // Configure RGB pins as OUTPUT
+    pinMode(RED_PIN, OUTPUT);   // TODO 5
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
-    // TODO 6:
     // Print initialization message
+    Serial.println("RGB PWM control initialized."); // TODO 6
 }
 
 void loop() {
 
     // -------- DIGITAL MODE --------
 
-    // TODO 7:
     // Turn ON red (digital HIGH)
+    digitalWrite(RED_PIN, HIGH); // TODO 7
+    Serial.println("Digital: RED ON");
+    delay(500);
 
-    // TODO 8:
     // Turn OFF red
+    digitalWrite(RED_PIN, LOW); // TODO 8
+    Serial.println("Digital: RED OFF");
+    delay(500);
 
     // -------- ANALOG (PWM) MODE --------
 
-    // TODO 9:
-    // Set RED brightness using analogWrite()
+    // Fade RGB using PWM to demonstrate analog brightness control
+    // This will smoothly transition colors by varying duty cycle
+    for (int brightness = 0; brightness <= 255; brightness += 5) {
+        // Set RED brightness using analogWrite()
+        analogWrite(RED_PIN, brightness);
+        // Set GREEN brightness using analogWrite()
+        analogWrite(GREEN_PIN, 255 - brightness);
+        // Set BLUE brightness using analogWrite()
+        analogWrite(BLUE_PIN, (brightness / 2));
 
-    // TODO 10:
-    // Set GREEN brightness using analogWrite()
+        // Add delay for visible transition
+        delay(20);
+    }
 
-    // TODO 11:
-    // Set BLUE brightness using analogWrite()
-
-    // TODO 12:
-    // Add delay for visible transition
+    // Reverse fade back to start for a smooth loop
+    for (int brightness = 255; brightness >= 0; brightness -= 5) {
+        analogWrite(RED_PIN, brightness);
+        analogWrite(GREEN_PIN, 255 - brightness);
+        analogWrite(BLUE_PIN, (brightness / 2));
+        delay(20);
+    }
 }
